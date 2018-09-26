e5702fc80c115b7021ccb948633c7b31c7df7af5
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-26 09:33:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,12 @@
 def reverse_each_word(str)
   new_array = string.split(" ")
   return_array = []
-  new_array.each do |w|
-  return_array << w.reverse
-end
+  new_array.each { |w| return_array << w.reverse }
   return_array.join(" ")
 end
 
 def reverse_each_word(str2)
   array2 = str2.split(" ")
   array2.collect { |word| puts word.reverse }
+  return array2
 end
